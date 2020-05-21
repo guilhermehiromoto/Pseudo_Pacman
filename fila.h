d1f29e53a544a817228dfcc0bd5cc23bd4c00697
Maria Fernanda <mafe-mello1@hotmail.com>
2020-05-21 16:40:29 -0300

    adicionando comentários...

--- fila.h
+++ fila.h
@@ -1,26 +1,27 @@
+/*
+ *          #####      GRUPO       ##### 
+ *
+ *	  Guilherme Amaral Hiromoto - nUSP: 11218959
+ *	  Maria Fernanda Lucio de Mello - nUSP: 11320860
+ *    Paulo Matana da Rocha - nUSP: 10892676
+ *    Victor Rodrigues Russo - nUSP: 11218855
+ *
+*/
+
 #ifndef FILA_H
 #define FILA_H
 
 typedef struct no NO;
 typedef struct fila FILA;
 
-
 FILA* fila_criar();
-
 void fila_apagar(FILA** fila);
-
 int fila_vazia(FILA* fila);
-
 int fila_comeco(FILA* fila);
-
 int fila_push(FILA* fila, int vertice);
-
 int fila_pop(FILA* fila);
-
 int fila_tamanho(FILA* fila);
-
 int fila_cheia(FILA* fila);
-
 int busca_fila(FILA* fila, int vertice);
 
 #endif
--- graph.h
+++ graph.h
@@ -1,20 +1,24 @@
+/*
+ *          #####      GRUPO       ##### 
+ *
+ *	  Guilherme Amaral Hiromoto - nUSP: 11218959
+ *	  Maria Fernanda Lucio de Mello - nUSP: 11320860
+ *    Paulo Matana da Rocha - nUSP: 10892676
+ *    Victor Rodrigues Russo - nUSP: 11218855
+ *
+*/
+
 #ifndef GRAFO_H
-	#define GRAFO_H
+#define GRAFO_H
 
-	typedef struct grafo GRAFO;
-	
-	GRAFO* criar_grafo();
-	
-	void inserir_aresta(GRAFO* graph, int i, int j);
+typedef struct grafo GRAFO;
 
-	void remover_aresta(GRAFO* graph, int i, int j);
-
-	void exibir_matriz(GRAFO* graph, int n);
-	
-	int get_matriz(GRAFO* graph,int i, int j);
-
-	void deleta_grafo(GRAFO* graph);
-
-	int* calculate_path(GRAFO* graph ,int v, int u);
+GRAFO* criar_grafo();
+void inserir_aresta(GRAFO* graph, int i, int j);
+void remover_aresta(GRAFO* graph, int i, int j);
+void exibir_matriz(GRAFO* graph, int n);
+int get_matriz(GRAFO* graph,int i, int j);
+void deleta_grafo(GRAFO* graph);
+int* calculate_path(GRAFO* graph ,int v, int u);
 
 #endif
